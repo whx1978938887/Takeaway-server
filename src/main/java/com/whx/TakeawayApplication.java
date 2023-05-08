911f4a56bcce60315d47cbad0969188fcbd98b40
wu <whx1978938887@student.gdufe.edu.cn>
2023-05-08 12:58:09 +0800

    create project

---
+++ src/main/java/com/whx/TakeawayApplication.java
@@ -0,0 +1,11 @@
+package com.whx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeawayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TakeawayApplication.class,args);
+    }
+}
